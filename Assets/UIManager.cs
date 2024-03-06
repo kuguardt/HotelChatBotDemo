3ac6c23f59ff4c358b8f8693e13a139b6c333e51
Kuguardt <kuguardt@gmail.com>
2024-03-06 14:20:19 -0800

    update failure checking

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,9 +2,12 @@ using Inworld;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    private int failcounter = 0;
+
     [SerializeField]
     private GameObject startPanel;
     [SerializeField]
@@ -17,16 +20,20 @@ public class UIManager : MonoBehaviour
     private GameObject faceScanPanel;
     [SerializeField]
     private GameObject showRoomInfoPanel;
+    [SerializeField]
+    private TextMeshProUGUI failCounterText;
+
 
     void Start()
     {
         DeactivePanel();
         startPanel.SetActive(true);
+        failCounterText.gameObject.SetActive(false);
     }
-
     public void SendText(string text)
     {
         InworldController.CurrentCharacter.SendText(text);
+        StartCoroutine(Delay(1.0f,text));
     }
 
     public void Emotion(string text1, string text2)
@@ -37,7 +44,13 @@ public class UIManager : MonoBehaviour
 
     public void GatherGoal(string goal)
     {
-        PanelToggle(goal);
+        //PanelToggle(goal);
+    }
+
+    public IEnumerator Delay(float time, string text)
+    {
+        yield return new WaitForSeconds(time);
+        PanelToggle(text);
     }
 
     private void PanelToggle(string goal)
@@ -52,23 +65,60 @@ public class UIManager : MonoBehaviour
                 DeactivePanel();
                 infoGatherPanel.SetActive(true);
                 break;
-            case "InformationGather":
+            case "InformationGatherSuccess":
                 DeactivePanel();
                 scanPassportPanel.SetActive(true);
+                failcounter = 0;
+                UpdateFailCounter(failcounter);
                 break;
-            case "ScanPassport":
+            case "InformationGatherFail":
+                DeactivePanel();
+                infoGatherPanel.SetActive(true);
+                UpdateFailCounter(++failcounter);
+                break;
+            case "ScanPassportSuccess":
                 DeactivePanel();
                 faceScanPanel.SetActive(true);
+                failcounter = 0;
+                UpdateFailCounter(failcounter);
                 break;
-            case "FaceScan":
+            case "ScanPassportFail":
+                DeactivePanel();
+                scanPassportPanel.SetActive(true);
+                UpdateFailCounter(++failcounter);
+                break;
+            case "FaceScanSuccess":
                 DeactivePanel();
                 showRoomInfoPanel.SetActive(true);
+                failcounter = 0;
+                UpdateFailCounter(failcounter);
+                break;
+            case "FaceScanFail":
+                DeactivePanel();
+                faceScanPanel.SetActive(true);
+                UpdateFailCounter(++failcounter);
+                break;
+            case "ShowRoomInformationDone":
+                DeactivePanel();
+                checkInPanel.SetActive(true);
                 break;
 
             default:
                 Debug.LogError("Goal is not valid : " + goal);
                 break;
         }
+    }
+
+    private void UpdateFailCounter(int amount)
+    {
+        if (amount <= 0)
+        {
+            failCounterText.gameObject.SetActive(false);
+            return;
+        }
+
+        failCounterText.gameObject.SetActive(true);
+        failCounterText.SetText("Fail count : " + amount.ToString());
     }
 
     public void DeactivePanel()
